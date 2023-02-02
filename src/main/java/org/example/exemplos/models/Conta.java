b02bdbc6067edfb3ff5e9626d2446083f0149623
Bruno Barros <brunog.barros@pm.me>
2023-02-01 21:46:14 -0300

    - adicionado dependencia do jUnit5 no pom.xml. - criada primeira classe de modelo Conta

---
+++ src/main/java/org/example/exemplos/models/Conta.java
@@ -0,0 +1,24 @@
+package org.example.exemplos.models;
+
+import java.math.BigDecimal;
+
+public class Conta {
+    private String pessoa;
+    private BigDecimal saldo;
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(String pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
